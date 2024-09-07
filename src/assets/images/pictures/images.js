ab58060ab05ce8c1d7eff4e128ba536f38fef4ef
Enyo Onuche <enyoonuche1@gmail.com>
2024-09-07 15:32:03 +0100

    Finished

--- src/assets/images/pictures/images.js
+++ src/assets/images/pictures/images.js
@@ -1,9 +1,13 @@
 import iphone from './iPhone13Pro.png'
 import singer from './singer.png'
 import gallery from './gallery.png'
+import appStoreBtn from './AppStoreFilled.png'
+import playStoreBtn from './GooglePlay.png'
 
 export default {
     iphone,
     singer,
-    gallery
+    gallery,
+    appStoreBtn,
+    playStoreBtn
 }
--- src/components/storeButtons.jsx
+++ src/components/storeButtons.jsx
@@ -1,22 +1,10 @@
-import { logos } from "../assets/images";
+import { pictures } from "../assets/images";
 
 const StoreButtons = () => {
-    return ( <div className="flex gap-5">
-            <div className="bg-black-100 rounded-md py-1 px-4 flex gap-3 text-white items-center border border-white">
-                <div><img width={32} height={32} src={logos.apple} alt="" /></div>
-                <div>
-                    <span className="text-xs">Download on the</span>
-                    <h4 className="text-xl font-medium">App store</h4>
-                </div>
-            </div>
-            <div className="bg-black-100 rounded-md py-1 px-4 flex gap-3 text-white items-center border border-white">
-                <div><img width={32} height={32} src={logos.apple} alt="" /></div>
-                <div>
-                    <span className="text-xs">Get it on</span>
-                    <h4 className="text-xl font-medium">Google Play</h4>
-                </div>
-            </div>
-    </div> );
+    return ( <>
+            <button className="w-fit"><img src={pictures.appStoreBtn} alt="" /></button>
+            <button><img src={pictures.playStoreBtn} alt="" /></button>
+    </> );
 }
  
 export default StoreButtons;
